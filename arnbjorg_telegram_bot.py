d03e2859601d35e6c1f72c11f659cacaa4b382bd
dad-siberian <dad.siberian@gmail.com>
2022-05-17 17:53:08 +0300

    add telegram bot

--- arnbjorg_telegram_bot.py
+++ arnbjorg_telegram_bot.py
@@ -33,29 +33,21 @@ async def echo(update: Update, context: CallbackContext.DEFAULT_TYPE) -> None:
 def detect_intent_texts(project_id, session_id, texts, language_code='ru-RU'):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
-    #print("Session path: {}\n".format(session))
     for text in texts.split(' '):
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
         query_input = dialogflow.QueryInput(text=text_input)
         response = session_client.detect_intent(
             request={"session": session, "query_input": query_input}
         )
-        # print("=" * 20)
-        # print("Query text: {}".format(response.query_result.query_text))
-        # print(
-        #     "Detected intent: {} (confidence: {})\n".format(
-        #         response.query_result.intent.display_name,
-        #         response.query_result.intent_detection_confidence,
-        #     )
-        # )
-        # print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
         return response.query_result.fulfillment_text
+
+
 
 
 def main() -> None:
 
     load_dotenv()
-    
+
     token = os.getenv('TELEGRAM_TOKEN')
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler('start', start))
